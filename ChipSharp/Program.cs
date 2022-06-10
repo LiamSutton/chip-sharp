6a0bd5e38b37c4d33d00e55dca1bb0f818f1afe0
Liam Sutton <liamsutton1997@outlook.com>
2022-06-10 22:39:38 +0100

    Create window

--- ChipSharp/Program.cs
+++ ChipSharp/Program.cs
@@ -1,5 +1,19 @@
-﻿using ChipSharp;
+﻿using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using Window = ChipSharp.Window;
 
-var rom = File.ReadAllBytes("Roms/IBMLogo.ch8");
-var vm = Vm.CreateVm(rom);
-vm.Run(false);
+var gameWindowSettings = new GameWindowSettings
+{
+    UpdateFrequency = 600
+};
+
+var nativeWindowSettings = new NativeWindowSettings
+{
+    Size = new Vector2i(1024, 512),
+    Profile = ContextProfile.Compatability,
+    Title = "Chip #"
+};
+
+var window = new Window(gameWindowSettings, nativeWindowSettings);
+window.Run();
